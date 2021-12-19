fe0a08d325df8c8d89b5701e2f26bc5985511804
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2021-12-19 15:35:02 +0300

    to do app with database

---
+++ index.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Task Manager</title>
+</head>
+<body>
+    <nav>
+        <ul>
+           <li> <a href="index.php">Home</a></li>
+           <li><a href="addtask.php">Add</a></li>
+           <li><a href="tasks.php">Tasks</a></li>
+        </ul>
+    </nav>
+    <div class="container" id="texts">
+       <div class="text">
+       <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Excepturi accusantium minus similique facere accusamus
+            rem aut eius. Culpa, repellendus? Quibusdam tempore 
+            doloremque sint unde dolores eius tempora rerum commodi omnis?
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Excepturi accusantium minus similique facere accusamus
+            rem aut eius. Culpa, repellendus? Quibusdam tempore 
+            doloremque sint unde dolores eius tempora rerum commodi omnis?
+        </p>
+        <a href="addtask.php"><button>ADD Task</button></a>
+       </div>
+    </div>
+</body>
+</html>
